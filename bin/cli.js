e4197474e79eb97fd7aa1e36cb5f3d8fabb86455
Jasper Vermeulen <jaspervermeulen1@gmail.com>
2018-02-08 14:09:11 +0100

    finish maf + init

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-const pkg = require( '../package.json' )
 const log = require( '../lib/log' )
 const Liftoff = require( 'liftoff' )
+const fetch = require( 'node-fetch' )
+const mkdirp = require( 'mkdirp' )
+const inquirer = require( 'inquirer' )
+const parseAuthor = require( 'parse-author' )
 const argv = require( 'minimist' )( process.argv.slice( 2 ) )
 const fs = require( 'fs' )
 const path = require( 'path' )
@@ -10,11 +13,14 @@ const { promisify } = require( 'util' )
 const { fork } = require( 'child_process' )
 const { createInterface } = require( 'readline' )
 
-require( 'please-upgrade-node' )( pkg )
+require( 'please-upgrade-node' )( require( '../package.json' ) )
 
+const fileType = `utf8`
 const SIGINT = `SIGINT`
 
 process.env.INIT_CWD = process.cwd()
+
+// TODO: all path resolve or similar
 
 const {
   readFile
@@ -22,12 +28,14 @@ const {
 , rename
 , readdir
 , stat
+, mkdir
 } = {
   readFile: promisify( fs.readFile )
 , writeFile: promisify( fs.writeFile )
 , rename: promisify( fs.rename )
 , readdir: promisify( fs.readdir )
 , stat: promisify( fs.stat )
+, mkdir: promisify( mkdirp )
 }
 
 const cli = new Liftoff( {
@@ -55,46 +63,133 @@ function start( env ) {
     console.log( `No ${this.configName} found.` )
   }
 
-  // TODO: test for metadata.json
+  const tasks = argv._
 
-  runSDK( env )
+  if ( tasks.includes( `init` ) )
+    initApp( env )
+  else
+    runSDK( env )
+}
+
+async function initApp( env ) {
+  const files = {}
+
+  const res = await fetch( `https://jsonip.metrological.com/?maf=true` )
+  const ipData = await res.json()
+  const language = ipData.geo.country.toLowerCase()
+
+  files[ `.mafrc.json` ] = JSON.stringify( { language }, null, 2 )
+
+  let pkg = {}
+  try { pkg = require( `${env.configBase}/package.json` ) } catch( e ) {}
+
+  const answers = await inquirer.prompt( [
+    { name: `name`, message: `App Name:`, default: pkg.name }
+  , { name: `version`, message: `App Version:`, default: pkg.version || `1.0.0` }
+  , { name: `author`, message: `Author:`, default: parseAuthor( pkg.author || `` ).name }
+  , { name: 'company', message: `Company:` }
+  , { name: 'description', message: `App Description:`, default: pkg.description }
+  , { name: 'categories', message: `App Categories:`, filter: answer => answer.split( /\W+/ ) }
+  ] )
+
+  files[ `Contents/metadata.json` ] = JSON.stringify(
+    {
+      identifier: env.configBase.split( `/` ).pop()
+    , name: answers.name
+    , version: answers.version
+    , author: answers.author
+    , company: answers.company
+    , copyright: answers.company
+    , description: answers.description
+    , categories: answers.categories
+    , scripts: `JavaScript/init.js`
+    , images: { icon: { '192x192': `Images/icon.png` } }
+    }
+  , null
+  , 2
+  )
+
+  const contents = `${env.configBase}/Contents`
+
+  await mkdir( contents )
+  await mkdir( `${contents}/Fonts` )
+  await mkdir( `${contents}/Images` )
+
+  fs.createReadStream( path.resolve( __dirname, `../lib/icon.png` ) )
+    .pipe( fs.createWriteStream( `${contents}/Images/icon.png` ) )
+  fs.createReadStream( path.resolve( __dirname, `../lib/icon.psd` ) )
+    .pipe( fs.createWriteStream( `${contents}/Images/icon.psd` ) )
+
+  await mkdir( `${contents}/Localization` )
+  await writeFile( `${contents}/Localization/en-EU.strings`, `// EN`, fileType )
+  await writeFile( `${contents}/Localization/${language}-EU.strings`, `// ${language.toUpperCase()}`, fileType )
+  await mkdir( `${contents}/JavaScript` )
+  await writeFile( `${contents}/JavaScript/init.js`, `// init`, fileType )
+  await writeFile( `${contents}/JavaScript/theme.js`, `// theme`, fileType )
+  await mkdir( `${contents}/JavaScript/Views` )
+  await mkdir( `${contents}/JavaScript/Core` )
+  await mkdir( `${contents}/JavaScript/Core` )
+  await writeFile( `${contents}/JavaScript/Core/api.js`, `// api`, fileType )
+
+  for ( file in files ) {
+    await writeFile(
+      `${env.configBase}/${file}`
+    , files[ file ]
+    , fileType
+    )
+  }
+
+  process.exit( 0 )
 }
 
 async function runSDK( env ) {
-  console.log( env.configPath )
-
   const mae = {
-    ui: 'com.metrological.ui.Demo'
-  , language: 'en'
+    ui: `com.metrological.ui.Demo`
+  , language: `en`
   , categories: [
-      'favorites'
-    , 'recently'
-    , 'video'
-    , 'news'
-    , 'social'
-    , 'games'
-    , 'sport'
-    , 'lifestyle'
+      `favorites`
+    , `recently`
+    , `video`
+    , `news`
+    , `social`
+    , `games`
+    , `sport`
+    , `lifestyle`
     ]
   , apps: []
   }
 
+  let metadata
+
+  try {
+    metadata = require( `${env.configBase}/metadata.json` )
+  } catch( e ) {
+    if ( e.code === `MODULE_NOT_FOUND` ) {
+      console.log( `metadata.json not found in: ${env.cwd}` )
+      process.exit( 1 )
+    } else {
+      throw e
+    }
+  }
+
   const html = '../node_modules/maf3-sdk/index.html'
-  const metadataPath = `${env.configBase}/metadata.json`
-  const metadata = require( metadataPath )
   const config = require( env.configPath )
-  const sdkApps = await dirs( path.resolve( __dirname, `../node_modules/maf3-sdk/apps/` ) )
+  const apps = await dirs( path.resolve( __dirname, `../node_modules/maf3-sdk/apps/` ) )
 
   fs.createReadStream( path.resolve( __dirname, `../lib/index.html` ) )
     .pipe( fs.createWriteStream( path.resolve( __dirname, html ) ) )
 
-  console.log( sdkApps )
-  console.log( config )
-  console.log( mae )
-  console.log( metadata )
+  apps.push( metadata.identifier )
 
-  // merge configs
-  // replace config in html - {{MAE}}
+  const merged = Object.assign( Object.assign( {}, mae ), config )
+  merged.categories = mae.categories.concat( metadata.categories )
+  merged.apps = apps
+
+  await replaceRegexInFile(
+    path.resolve( __dirname, html )
+  , /{{MAE}}/gi
+  , `var MAE = ${JSON.stringify( merged, null, 2 )}`
+  )
 
   fork( require.resolve( `maf3-sdk` ), [], { cwd: process.cwd() } )
 }
@@ -106,6 +201,18 @@ async function dirs( root ) {
     return stats.isDirectory()
   } ) )
   return dirs.filter( dir => dir.includes( 'com.metrological.app.' ) )
+}
+
+async function replaceRegexInFile( file, search, replace ) {
+
+  const contents = await readFile( file, fileType )
+  const replaced = contents.replace( search, replace )
+  const tmpfile = `${file}.jstmpreplace`
+
+  await writeFile( tmpfile, replaced, fileType )
+  await rename( tmpfile, file )
+
+  return true
 }
 
 function stop( code ) {
