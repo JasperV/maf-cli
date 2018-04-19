0b2b8ce5981a3cd27498349b28c2839fad0c0744
Jasper Vermeulen <jaspervermeulen1@gmail.com>
2018-04-19 15:56:19 +0200

    install fix

--- scripts/install.js
+++ scripts/install.js
@@ -12,7 +12,11 @@ const writeFile = promisify( fs.writeFile )
 async function getTranslation() {
   const res = await fetch( `https://dashboard.metrological.com/locales/en/translation.json` )
   const translation = await res.json()
-  writeFile( path.resolve( `../`, `lib/resources/translation.json` ), JSON.stringify( translation, null, 2 ) )
+
+  writeFile(
+    path.resolve( process.cwd(), `../maf-cli/lib/resources/translation.json` )
+  , JSON.stringify( translation, null, 2 )
+  )
 }
 
 getTranslation()
